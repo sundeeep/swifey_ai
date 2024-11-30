83e91ee38d40dc3cb8e46b97265a76cdd143a526
sundeeep <sandeepkumar25820@gmail.com>
2024-11-30 11:58:50 +0530

    NativeWind Config & Adding Assets Done!

--- swifey_app/app/_layout.tsx
+++ swifey_app/app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from 'expo-router'
+// Importing global CSS file
+import "../global.css";
 
 const RootLayout = () => {
   return (
     <Stack>
         <Stack.Screen name="index" options={{
+            headerShown: false,
+        }}/>
+
+        <Stack.Screen name="profile" options={{
             headerShown: false,
         }}/>
     </Stack>
--- swifey_app/app/index.tsx
+++ swifey_app/app/index.tsx
@@ -1,27 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 
 const App = () => {
 
   return (
-    <View style={styles.container}>
-      <Text>Swifey.ai</Text>
+    <View className='flex-1 items-center justify-center bg-white gap-5'>
+      <Text className='font-bold text-5xl'>Swifey.ai</Text>
 
-      <Link href='/profile' style={{
-        color: 'blue',
-      }}>Go to Profile</Link>
+      <Link href='/profile' className='underline font-semibold text-blue-600'>Go to Profile</Link>
     </View>
   )
 }
 
 export default App;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
